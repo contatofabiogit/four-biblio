0c3d2112fde83a1db4cb6cc4b784e9c5b70652af
Fábio Alves Ribeiro <contato.fabioalves@outlook.com>
2016-11-30 07:45:20 -0200

    Dia 30/11: Commit 01 - Começando a pagina inicial

--- cad_usuario.php
+++ cad_usuario.php
@@ -6,13 +6,13 @@
         <title>Four Biblio</title>
 
         <?php include_once './padroes/cabecalho_head.php'; ?>
-
+        <link rel="stylesheet" href="./css/design.css" />
     </head>
     <body>
-        <div class="container">
+        <div class="container principal">
             <div class="row">
                 <header>
-                    
+                    <img class="img-responsive" src="img/bem-vindo.png" />
                 </header>
             </div>
         </div>
